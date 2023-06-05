05b5f39d7cb7c2a396bcaf9e518e7483b97d8dd3
dtuanv <dtuanlieb@gmail.com>
2023-06-05 16:19:11 +0200

    [+] Add Header and dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChatBotComponent } from '../chat-bot/chat-bot.component';
+import { ChatBotComponent } from './chat-bot/chat-bot.component';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ChatBotComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ChatBotComponent,
+        DashboardComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        FormsModule,
+        HeaderComponent
+    ]
 })
 export class AppModule { }

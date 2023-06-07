8ddb27522c0facc879c99e7100fbbdec26bddb9b
dtuanv <dtuanlieb@gmail.com>
2023-06-07 15:49:02 +0200

    [+] connect Chat_Bot with Backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ChatBotComponent } from './chat-bot/chat-bot.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: 'home', pathMatch: 'full'},
   {path:'', component: LoginComponent},
 
   {path:'chat', component: ChatBotComponent},
-  {path:'login', component: LoginComponent}
+  {path:'login', component: LoginComponent},
+  {path:'admin', component: DashboardComponent}
 ];
 
 @NgModule({
--- src/app/chat-bot/chat-bot.component.ts
+++ src/app/chat-bot/chat-bot.component.ts
@@ -1,9 +1,12 @@
 import { Component, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 interface Message {
   sender: string;
   content: string;
 }
+
 
 @Component({
   selector: 'app-chat-bot',
@@ -11,11 +14,22 @@ interface Message {
   styleUrls: ['./chat-bot.component.css']
 })
 export class ChatBotComponent {
+  constructor(private http: HttpClient) { }
   messageQueue: any[] = [];
   userMessage: string = '';
   botMessage: string = '';
+  botAnswer: any = {};
+  messageObject: any = {};
+  botSteps: any[] = []
 
-  sendMessage(user: string) {
+
+  indexStep = 0
+
+
+
+
+
+  sendMessage(user: string): void {
     let message = '';
     if (user === 'User') {
       message = this.userMessage;
@@ -25,13 +39,53 @@ export class ChatBotComponent {
       this.botMessage = '';
     }
 
-    const messageObject = {
-      user: user,
-      message: message
-    };
+    // const messageObject = {
+    //   user: user,
+    //   message: message
 
-    this.messageQueue.push(messageObject);
+    // };
 
+    // this.messageQueue.push(messageObject);
+
+    const inputToBot = {
+      input: message,
+      state: 'hi'
+    }
+
+    this.http.post('http://localhost:8080/it_chat/', inputToBot).subscribe(
+      (response) => {
+        // Handle the response
+        this.botAnswer = response
+        console.log(" this.botAnswer ", response);
+        if (this.botAnswer.label) {
+          this.messageObject = {
+            user: user,
+            message: message,
+            answer: this.botAnswer.answer
+          };
+          this.messageQueue.push(this.messageObject);
+
+        }
+        if (this.botAnswer.name) {
+          this.messageObject = {
+            user: user,
+            message: message,
+            initial_answer: this.botAnswer.initial_answer,
+            network: this.botAnswer.network,
+            steps: this.botAnswer.steps
+          }
+          this.botGetStep(this.botAnswer.steps)
+          this.messageQueue.push(this.messageObject);
+
+        }
+
+
+      },
+      (error) => {
+        // Handle any errors
+        console.error(error);
+      }
+    );
     // Reset the input fields
     if (user === 'User') {
       this.userMessage = '';
@@ -40,7 +94,68 @@ export class ChatBotComponent {
     }
   }
 
-  clearTheChat(){
+  botGetStep(steps: any) {
+    console.log("this.indexStep ", this.indexStep)
+    this.botSteps.push(steps[this.indexStep])
+    console.log("this.botSteps ", this.botSteps)
+    return this.botSteps
+  }
+
+  stepAnswer(answer: any, steps: any, stepNum: any) {
+    if (answer == 'failure') {
+      this.indexStep++
+      this.botGetStep(steps)
+    }
+    if (answer == 'success') {
+      let inputSuccess = { state: "problem_solved" }
+      this.http.post('http://localhost:8080/it_chat/', inputSuccess).subscribe(
+        (response) => {
+          // Handle the response
+          this.botAnswer = response
+          console.log(" this.botAnswer ", response);
+          if (this.botAnswer.answer) {
+            this.messageObject = {
+
+              answer: this.botAnswer.answer
+            };
+            this.messageQueue.push(this.messageObject);
+
+          }
+        },
+        (error) => {
+          // Handle any errors
+          console.error(error);
+        }
+      );
+    }
+
+    if(answer == 'unable'){
+      let inputUnable = { state: "problem_solved" }
+      this.http.post('http://localhost:8080/it_chat/', inputUnable).subscribe(
+        (response) => {
+          // Handle the response
+          this.botAnswer = response
+          console.log(" this.botAnswer ", response);
+          if (this.botAnswer.answer) {
+            this.messageObject = {
+
+              answer: this.botAnswer.answer
+            };
+            this.messageQueue.push(this.messageObject);
+
+          }
+        },
+        (error) => {
+          // Handle any errors
+          console.error(error);
+        }
+      );
+    }
+  }
+
+
+
+  clearTheChat() {
     this.messageQueue = [];
   }
 

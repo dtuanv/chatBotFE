10e5856a2630d3fc742a28e30015b2078e316165
dtuanv <dtuanlieb@gmail.com>
2023-06-07 18:16:10 +0200

    [+] Add Admin Component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent  {
+export class DashboardComponent {
+  constructor(private http: HttpClient) { }
+  questions: any = []
 
   ngOnInit(): void {
     console.log("local ", localStorage.getItem('role'))
+
+    this.http.get('http://localhost:8614/getQuestion').subscribe(
+      (response) => {
+
+        this.questions = response
+
+        console.log(response);
+        // Handle the response data here
+      },
+      (error) => {
+        console.error(error);
+        // Handle errors here
+      }
+    );
+
   }
+
+
 }
